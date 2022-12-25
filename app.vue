89e8cf69a27445f4cc73acd8d44eec3e4c8fdafe
Aleks <aleksandar.trpkovski@gmail.com>
2022-12-26 10:41:19 +1100

    Added TailwindCSS

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@ const story = await useAsyncStoryblok("home", { version: "draft" });
 </script>
 <template>
     <main role="main">
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
         <pre>{{ story }}</pre>
         <!-- <StoryblokComponent v-if="story" :blok="story.content" /> -->
     </main>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 export default defineNuxtConfig({
+    css: ["~/assets/css/main.css"],
     modules: ["@storyblok/nuxt"],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
     storyblok: {
         accessToken: process.env.STORYBLOK_API_TOKEN,
         useApiClient: true,
